350518695e7701178f88d8c1327fea024ae1ab28
Ana Vitória <81657874+naviisouza@users.noreply.github.com>
2023-12-15 21:13:57 -0300

    Update floyd-warshall.py

--- floyd-warshall.py
+++ floyd-warshall.py
@@ -29,21 +29,4 @@ def printSolution(dist):
             if j == V-1:
                 print()
 
-
-if __name__ == "__main__":
-    """
-                    10
-            (0)------->(3)
-                |        /|\
-            5   |         |
-                |         | 1
-            \|/          |
-            (1)------->(2)
-                    3         """
-    graph = [[0, 5, INF, 10],
-             [INF, 0, 3, INF],
-             [INF, INF, 0, 1],
-             [INF, INF, INF, 0]
-             ]
-
     floydWarshall(graph)
