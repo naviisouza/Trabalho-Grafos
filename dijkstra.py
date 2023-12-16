178bdd39da3bb6b27b26d0fdac02d55393d393cd
Ana Vitória <81657874+naviisouza@users.noreply.github.com>
2023-12-15 21:49:01 -0300

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -48,21 +48,5 @@ class Graph():
         # Imprimir a solução final
         self.printSolution(dist)
 
-
-if __name__ == "__main__":
-    # Criar um objeto da classe Graph
-    g = Graph(9)
-    # Definir a matriz de adjacência do grafo
-    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-               [4, 0, 8, 0, 0, 0, 0, 11, 0],
-               [0, 8, 0, 7, 0, 4, 0, 0, 2],
-               [0, 0, 7, 0, 9, 14, 0, 0, 0],
-               [0, 0, 0, 9, 0, 10, 0, 0, 0],
-               [0, 0, 4, 14, 10, 0, 2, 0, 0],
-               [0, 0, 0, 0, 0, 2, 0, 1, 6],
-               [8, 11, 0, 0, 0, 0, 1, 0, 7],
-               [0, 0, 2, 0, 0, 0, 6, 7, 0]
-               ]
-
     # Executar o algoritmo de Dijkstra com a fonte no vértice 0
     g.dijkstra(0)
